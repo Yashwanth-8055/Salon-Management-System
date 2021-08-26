17a5602a31b16556f87b5fa26421a23f28af6cfa
Yashwanth-8055 <64087633+Yashwanth-8055@users.noreply.github.com>
2021-08-26 21:18:04 +0530

    Add files via upload

---
+++ salon/EasyBarber/forms.py
@@ -0,0 +1,42 @@
+#This file is created to have a login/signup page for new customers
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+from .models import *
+
+
+
+
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+
+	    model = User
+	    fields = ["username", "email", "password1", "password2","first_name","last_name"]
+
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['app_no','shop_name','emp_name','rating','comment']
+
+
+
+class PayNow(ModelForm):
+
+
+
+    class Meta:
+        model = Payment
+        fields = ['pay_id','app_no','amt']
+
+
+
+
+
